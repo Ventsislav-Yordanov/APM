36228f0e54c17b6e73617062b2a47e5b3504dde5
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-11-25 11:02:32 +0200

    Fixed little bug with the AppComponent's template.
    Also remove some text from the index page.

--- app/app.component.js
+++ app/app.component.js
@@ -8,14 +8,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.pageTitle = "Acme Product Management";
     }
     AppComponent = __decorate([
         core_1.Component({
-            selector: 'pm-app',
-            template: "\n        <h1>Angular2: Getting Started</h1>\n    "
+            selector: "pm-app",
+            template: "\n    <div>\n        <h1>{{pageTitle}}</h1>\n        <div>My first component</div>\n    </div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
     selector: "pm-app",
     template: `
     <div>
-        </h1>{{pageTitle}}<h1>
+        <h1>{{pageTitle}}</h1>
         <div>My first component</div>
     </div>
     `
