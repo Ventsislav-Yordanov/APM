d15d7937164a94522e5eb261e49790870c92aba6
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-11-26 05:37:10 +0200

    Binded the product imageUrl
    Binded the product imageUrl, productName and some styles in img tag in
    the ProductListComponent's template

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -12,6 +12,8 @@ var core_1 = require("@angular/core");
 var ProductListComponent = (function () {
     function ProductListComponent() {
         this.pageTitle = "Product List";
+        this.imageWidth = 50;
+        this.imageMargin = 2;
         this.products = [
             {
                 "productId": 1,
