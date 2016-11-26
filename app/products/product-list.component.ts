e6f764619472b135ab98f08fba20b2ba59a24d34
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-11-26 04:10:27 +0200

    Added the product-list.component.ts

---
+++ app/products/product-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: "pm-products",
+    templateUrl: "app/products/product-list.component.html"
+})
+
+export class ProductListComponent { }
